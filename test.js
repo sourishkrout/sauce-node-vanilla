0f07e85f1390edc77e256f751766c9ffc375f93a
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2012-11-07 16:59:32 -0800

    Use the canonical test app for the vanilla demo.

--- test.js
+++ test.js
@@ -8,10 +8,9 @@ var config = {
       , username: process.env.SAUCE_USERNAME
       , accessKey: process.env.SAUCE_ACCESS_KEY
       , caps: [
-          { browserName: "firefox", version: "14", platform: "linux", proxy: { proxyType: "direct" } }
+        { browserName: "firefox", version: "14", platform: "", proxy: { proxyType: "direct" } }
         //, { browserName: "chrome", version: "", platform: "VISTA", proxy: { proxyType: "direct" } }
-        //, {browserName: "ipad", version: '', platform: "Mac 10.6", deviceOrientation: "landscape", proxy: {proxyType: 'direct'}}
-        //, {browserName: "android", version: '4', platform: "linux", deviceType: "tablet", proxy: {proxyType: 'direct'}}
+        //  {browserName: "ipad", version: '', platform: "Mac 10.6", deviceOrientation: "landscape", proxy: {proxyType: 'direct'}}
       ]
     };
 
@@ -19,13 +18,63 @@ var config = {
   var wdi = wd.remote(config.host, config.port, config.username, config.accessKey);
 
   var test = function(driver, cap, callback) {
-    cap.name = "I am a large guinea pig";
+    cap.name = "I am a large guinea pig using a " + cap.browserName;
     driver.init(cap, function() {
-      driver.get("https://saucelabs.com/test/guinea-pig", function() {
-        driver.elementByName("comments", function(err, elem) {
-          driver.type(elem, "Guinea pigs love to write automated tests.", function() {
-            driver.quit(function() {
-              callback();
+      login(driver, function() {
+        post(driver, function() {
+          driver.quit(function() {});
+        });
+      });
+    });
+  };
+
+  var post = function(driver, callback) {
+    var TITLE = "Guinea pigs are fun";
+    driver.get("http://tutorialapp.saucelabs.com/idea_add", function() {
+      driver.elementByName("title", function(err, _title) {
+        _title.type(TITLE, function() {
+          driver.elementByName("tags", function(err, _tags) {
+            _tags.type("automated testing", function() {
+              driver.elementByName("text", function(err, _text) {
+                _text.type("We should have more guinea pigs everywhere. They are fun and love writing automated tests!", function() {
+                  driver.elementByName("form.submitted", function(err, _submit) {
+                    _submit.click(function() {
+                      driver.elementByCss("div#sections h1", function(err, _h1) {
+                        _h1.text(function(err, _text) {
+                          assert.equal(_text, TITLE);
+                          callback();
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  };
+
+  var login = function(driver, callback) {
+    driver.get("http://tutorialapp.saucelabs.com/", function() {
+      driver.elementByName("login", function(err, _login) {
+        _login.type("guineapiggy", function() {
+          driver.elementByName("password", function(err, _password) {
+            _password.type("guineapiggy", function() {
+              driver.elementByName("submit", function(err, _submit) {
+                _submit.click(function(err) {
+                  driver.elementById("message", function(err, _message) {
+                    _message.text(function(err, _text) {
+                      assert.equal(_text, "Logged in successfully.");
+                      driver.waitForElementByLinkText("What's your idea?", 10, function(err) {
+                        assert.equal(err, null);
+                        callback();
+                      });
+                    });
+                  });
+                });
+              });
             });
           });
         });
